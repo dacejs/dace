ad4e9a495ee3cd4554101d11007f5be1bee18742
zhongzhi107 <zhong.zhi@163.com>
2018-06-06 10:59:50 +0800

    chore: 设置首页网页标题

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 const Home = () => (
   <div className="center-align" style={{ marginTop: '200px' }}>
+    <Helmet>
+      <title>Home</title>
+    </Helmet>
     <h3>Welcome</h3>
     <p>Check out these awesome features</p>
   </div>
