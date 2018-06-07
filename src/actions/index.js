13f0dfb1b50eda24d62cf54efa8193888e5089e7
zhongzhi107 <zhong.zhi@163.com>
2018-06-07 19:52:18 +0800

    chore: 删除 FETCH_CURRENT_USER action

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,16 +13,6 @@ export const fetchUsers = () => async (dispatch, getState, api) => {
   }
 };
 
-export const FETCH_CURRENT_USER = 'fetch_current_user';
-export const fetchCurrentUser = () => async (dispatch, getState, api) => {
-  const res = await api.get('/current_user');
-
-  dispatch({
-    type: FETCH_CURRENT_USER,
-    payload: res
-  });
-};
-
 export const FETCH_ADMINS = 'fetch_admins';
 export const fetchAdmins = () => async (dispatch, getState, api) => {
   const res = await api.get('/admins');
