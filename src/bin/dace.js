c94653471884e3f1cfbbe458728bd5e36e383b14
zhongzhi107 <zhong.zhi@163.com>
2018-07-13 11:49:50 +0800

    chore: 重命名命令名 dace start -> dace dev

--- src/bin/dace.js
+++ src/bin/dace.js
@@ -6,7 +6,7 @@ const logo = require('../utils/logo');
 const { version } = require('../../package.json');
 
 program
-  .command('start', '启动本地服务', { isDefault: true })
+  .command('dev', '启动本地服务', { isDefault: true })
   .command('build', '编译项目')
   .option('-l, --no-logo', '隐藏logo图标')
   .parse(process.argv);
