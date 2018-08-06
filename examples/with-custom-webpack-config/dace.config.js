20618134d9df8eac81620f34e16a285b62663096
zhongzhi107 <zhong.zhi@163.com>
2018-08-06 18:45:43 +0800

    release: v1.0.0-beta.10

--- examples/with-custom-webpack-config/dace.config.js
+++ examples/with-custom-webpack-config/dace.config.js
@@ -4,7 +4,7 @@ module.exports = {
 
     // 修改开发环境下浏览器端编译输出文件的名称
     if (target === 'web' && dev) {
-      appConfig.output.filename = 'client.custom.js';
+      appConfig.mode = 'production';
     }
 
     return appConfig;
