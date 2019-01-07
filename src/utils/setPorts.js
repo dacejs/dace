8680a6c4ed913490fa7122f7f44283f665a19c7a
zhongzhi107 <zhong.zhi@163.com>
2019-01-07 14:33:37 +0800

    fix: 从环境变量中获取devServer的端口号

--- src/utils/setPorts.js
+++ src/utils/setPorts.js
@@ -2,6 +2,10 @@ import { choosePort } from 'react-dev-utils/WebpackDevServerUtils';
 
 // Checks if PORT and PORT_DEV are available and suggests alternatives if not
 export default async () => {
-  await choosePort('localhost', 3000);
-  await choosePort('localhost', 3001);
+  const { DACE_HOST, DACE_PORT } = process.env;
+  const webserverPort = parseInt(DACE_PORT, 10);
+  const staticPort = webserverPort + 1;
+
+  await choosePort(DACE_HOST, webserverPort);
+  await choosePort(DACE_HOST, staticPort);
 };
