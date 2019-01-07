8fc06865b7144dd57a771766130451b217e26a46
zhongzhi107 <zhong.zhi@163.com>
2019-01-07 14:34:00 +0800

    chore: 删除无用的文件

--- src/utils/typeof.js
+++
@@ -1,3 +0,0 @@
-export const isFunction = obj => typeof obj === 'function';
-export const isObject = obj => obj !== null && typeof obj === 'object';
-export const isPromise = value => isObject(value) && isFunction(value.then);
