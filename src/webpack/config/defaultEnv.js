aa074c6b57ff42203eaf07ea7af0d12a10e84f8f
zhongzhi107 <zhong.zhi@163.com>
2019-01-07 15:00:54 +0800

    feat: 可以在 dace.config.js 中配置环境变量和路径变量

--- src/webpack/config/defaultEnv.js
+++ src/webpack/config/defaultEnv.js
@@ -1,21 +1,39 @@
-const paths = require('./paths');
+import fs from 'fs';
+import path from 'path';
+import { appStatsJson } from './paths';
 
-module.exports = {
-  // 客户端编译输出的 stats 文件位置
-  DACE_STATS_JSON: paths.appStatsJson,
+const appDaceConfig = path.resolve('dace.config.js');
+const daceConfig = fs.existsSync(appDaceConfig) ? require(appDaceConfig) : {};
 
+const defaultEnv = {
   // 本地开发 web server 主机名
   DACE_HOST: 'localhost',
 
   // 本地开发 web server 端口
-  DACE_PORT: 3000,
-
-  // API 接口地址 BaseURL，建议配置在 profiles 中
-  DACE_API_BASE_URL: 'http://localhost:3002',
+  DACE_PORT: '3000',
 
   // 编译产物对外服务访问使用的 URL
   DACE_PUBLIC_PATH: '/',
 
-  // 编译产物输出目录位置
-  DACE_BUILD_PATH: paths.appClientBuild
+  // 客户端编译输出的 stats 文件位置
+  DACE_STATS_JSON: appStatsJson,
+
+  // API 接口地址 BaseURL，建议配置在 profiles 中
+  // 仅在 dace-plugin-redux 中使用
+  DACE_API_BASE_URL: 'http://localhost:3002',
+
+  // 页面切换后是否自动滚动到页面顶部
+  DACE_SCROLL_TO_TOP: true,
+
+  // 路由默认使用的首页文件名称
+  DACE_INDEX: 'index'
 };
+
+// 让 dace.config.js 覆盖 defaultEnv.js 中的配置
+Object.keys(defaultEnv).forEach((key) => {
+  if (key in daceConfig) {
+    defaultEnv[key] = daceConfig[key];
+  }
+});
+
+export default defaultEnv;
