82654b309a9fde41c12af09aa3fdb32116c7e23f
zhi.zhong <zhong.zhi@163.com>
2019-06-13 10:59:13 +0800

    chore: Fix eslint error

--- examples/feat-custom-404/src/components/NotFound.js
+++ examples/feat-custom-404/src/components/NotFound.js
@@ -10,7 +10,10 @@ import PropTypes from 'prop-types';
 const NotFound = ({ location }) => (
   <div>
     <h1>feat-custom-404</h1>
-    <h2>Ooops, No match for <code>{location.pathname}</code></h2>
+    <h2>
+      Ooops, No match for
+      <code>{location.pathname}</code>
+    </h2>
   </div>
 );
 
