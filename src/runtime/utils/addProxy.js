09f6236cefaabdb9f8279867bd9e7343c2bd3b22
zhi.zhong <zhong.zhi@163.com>
2019-11-23 10:07:24 +0800

    Update addProxy.js

--- src/runtime/utils/addProxy.js
+++ src/runtime/utils/addProxy.js
@@ -6,7 +6,7 @@ export default (app) => {
     try {
       rules = JSON.parse(process.env.DACE_PROXY);
       app.use(urlrewrite(rules, {
-        debug: true
+        debug: process.env.NODE_ENV === 'development'
       }));
     } catch (e) {
       throw new Error('[JSON.parse error] `DACE_PROXY` is an invalid json.');
